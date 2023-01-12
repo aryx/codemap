8f9372bd116f8091d670cf1b08ce0c22fe282083
pad <pad@r2c.dev>
2023-01-12 09:45:11 +0100

    update to latest pfff

--- lang_nw/parsing/test_parsing_nw.ml
+++ lang_nw/parsing/test_parsing_nw.ml
@@ -19,11 +19,13 @@ let test_parse_nw file =
   Error_code.try_with_print_exn_and_reraise file (fun () ->
       Parse_nw.parse file |> ignore)
 
+(*
 let test_dump_nw file =
   let (trees, _toks), _stat = Parse_nw.parse file in
   let v = Meta_ast_fuzzy.vof_trees trees in
   let s = OCaml.string_of_v v in
   pr2 s
+ *)
 
 (*****************************************************************************)
 (* Unit tests *)
@@ -37,5 +39,7 @@ let actions () =
   [
     ("-tokens_nw", "   <file>", Common.mk_action_1_arg test_tokens_nw);
     ("-parse_nw", "   <file>", Common.mk_action_1_arg test_parse_nw);
+(*    
     ("-dump_nw", "   <file>", Common.mk_action_1_arg test_dump_nw);
+ *)
   ]
