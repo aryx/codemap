d648667ff7f040c5cd40e8c66d99231c54907f9d
pad <pad@r2c.dev>
2025-04-01 14:25:33 +0200

    deleted lib_parsing_html.ml and test_parsing_html.ml
    see r2c/TODO-code/ if needed

--- languages/html/lib_parsing_html.mli
+++
@@ -1,4 +0,0 @@
-val find_html_files_of_dir_or_files : 
-  string list (* files or dirs *) -> string (* filename *) list
-val html_tree_to_html : Ast_html.html_tree -> Ast_html.html
-val get_data_any : Ast_html.any -> string list
